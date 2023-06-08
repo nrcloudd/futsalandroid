8b5e7765bcefc552b8779a1e86f2e1cb37f38f82
dfitriana28 <111998066+dfitriana28@users.noreply.github.com>
2023-06-08 13:18:51 +0700

    Create splash_page.dart

---
+++ flutter_futsal/lib/splash_page.dart
@@ -0,0 +1,62 @@
+import 'login.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  late Animation<double> opacity;
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+        duration: Duration(milliseconds: 2500), vsync: this);
+    opacity = Tween<double>(begin: 1.0, end: 0.0).animate(controller)
+      ..addListener(() {
+        setState(() {});
+      });
+    controller.forward().then((_) {
+      navigationPage();
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  void navigationPage() {
+    Navigator.of(context)
+        .pushReplacement(MaterialPageRoute(builder: (_) => Login()));
+  }
+
+  Widget build(BuildContext context) {
+    return Container(
+      // decoration: BoxDecoration(
+      //     image: DecorationImage(
+      //         image: AssetImage('assets/logo.png'), fit: BoxFit.cover)),
+      child: Container(
+        decoration: BoxDecoration(color: Colors.white),
+        child: SafeArea(
+          child: new Scaffold(
+            body: Column(
+              children: <Widget>[
+                Expanded(
+                  child: Opacity(
+                      opacity: opacity.value,
+                      child: new Image.asset('assets/images/logo.png')),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
