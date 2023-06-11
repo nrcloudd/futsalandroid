7c0fe4dd238430ac78f3dff133607a0095cdbe03
Ghzxxx <Dzaghazy08@gmail.com>
2023-06-11 19:53:29 +0700

    Update login API

--- flutter_futsal/lib/api_connection/api_connection.dart
+++ flutter_futsal/lib/api_connection/api_connection.dart
@@ -25,5 +25,24 @@ class API {
       return 'Registration failed '; // Replace with your failure message
     }
   }
+
+  static Future<String> loginUser(String emailMember, String passMember) async {
+    final response = await http.post(
+      Uri.parse('$Connect/api/login'),
+      body: {
+        'emailMember': emailMember,
+        'passMember': passMember,
+      },
+    );
+    if (response.statusCode == 200) {
+      // Successful login
+      final token = response.body; // Assuming the response contains a token
+      return token;
+    } else {
+      // Login failed
+      return "Login failed";
+    }
+  }
+
 }
 
